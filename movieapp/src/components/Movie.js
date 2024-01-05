047c2f47e187c2cec7d00081fa82001a8a492d46
홍길동 <fofn0766@gmail.com>
2024-01-05 15:46:47 +0900

    Detail.js h/w

--- movieapp/src/components/Movie.js
+++ movieapp/src/components/Movie.js
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
-function Movie({medium_cover_image, title, summary, genres}){
+function Movie({id, medium_cover_image, title, summary, genres}){
     return <div>
     <img src = {medium_cover_image} alt = "IMG"></img>
     <h2>
-    <Link to = "/movie">{title}</Link>
+    <Link to = {`/movie/${id}`}>{title}</Link>
     </h2>
     <p>{summary}</p>
     <ul>{genres.map((g) => (
@@ -15,6 +15,7 @@ function Movie({medium_cover_image, title, summary, genres}){
 }
 
 Movie.propTypes = {
+  id : PropTypes.number.isRequired,
   medium_cover_image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
